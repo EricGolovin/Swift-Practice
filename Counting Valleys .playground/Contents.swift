4f95326b534a057200ad4eb82d2b8bf83cb73352
Eric Golovin <ericgolovin@outlook.com>
2020-02-27 22:03:46 +0200

    added hiking task

--- Counting Valleys .playground/Contents.swift
+++ Counting Valleys .playground/Contents.swift
@@ -1,27 +1,14 @@
 func countingValleys(n: Int, s: String) -> Int {
-    var numOfValleys = 0
-    var flag = false
-    var index: Int = 0 {
-        willSet {
-            if newValue < 0 {
-                flag = true
-            }
-        }
-        didSet {
-            if oldValue == 0 && flag {
-                numOfValleys += 1
-            }
-        }
-    }
-    
+    var numOfValleys = 0, index = 0
     for letter in s {
         if letter == "U" {
             index += 1
         } else if letter == "D" {
             index -= 1
+            if index + 1 == 0 {
+                numOfValleys += 1
+            }
         }
-        print("Index = \(index) letter = \(letter)")
-        
     }
     return numOfValleys
 }
